1d7ac9bdc79f30c5a4431d0ca11303c4298a460b
Christopher Rodriguez <cdr255@gmail.com>
2012-02-18 16:26:26 -0500

    Finished up LazyFoo's First Tutorial...

--- main.cpp
+++ main.cpp
@@ -6,10 +6,37 @@ int main(int argc, char *argv[]) {
 
 	// Two screens: One for an image (hello) and
 	// one for the visible window (screen). Initialized
-	// to NULL, to be safe.
-	
+	// these POINTERS to NULL, to be safe.
 	SDL_Surface * hello = NULL;
 	SDL_Surface * screen = NULL;
+
+	// Start ALL SDL Subsystems.
+	SDL_Init( SDL_INIT_EVERYTHING );
+
+	// Set Up the Window where the Action will take place,
+	// using the pointer defined above for that purpose.
+	screen = SDL_SetVideoMode( 640, 480, 32, SDL_SWSURFACE );
+
+	// Use the other pointer to load an image.
+	hello = SDL_LoadBMP( "hello.bmp" );
+
+	// Since screen is the window, we need to draw hello onto
+	// it (Blitting).
+	SDL_BlitSurface( hello, NULL, screen, NULL );
+
+	// Now, since the screen has changed, it needs to be flipped.
+	SDL_Flip( screen );
+
+	// Delay, for didactic purposes. (in milliseconds)
+	SDL_Delay( 2000 );
+
+	// Now is the time for cleanup. We need to free the loaded image...
+	SDL_FreeSurface( hello );
+
+	// And Quit SDL.
+	SDL_Quit();
+
+
 	return 0;
 }
 
